40188de2edac5ef14b2163562290b0cac660ec33
fedallxn <faithe.dall@gmail.com>
2025-08-30 15:41:01 -0500

    I added two more constant variables to expand the adventure game further. Added more conditions if they choose the village and propmts them for another response!

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -28,16 +28,36 @@ const readline = require('readline-sync');
 
 const hasTorch = true;
 const hasMap = false;
+const hasWeapon = true;
+const hasCharisma = true;
 
 console.log("You see two paths: one leads to the mountains, the other to the village.");
 const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
 
 if (choice === "mountains" && hasTorch) {
-  console.log("You safely navigate through the dark mountains.");
+  console.log("You safely navigate through the dark mountains. But there are creatures ahead.");
+  if (hasWeapon === true) {
+    console.log("Good thing you have this awesome weapon to fight with!");
+  } else {
+    console.log("Too bad you don't have a weapon! Maybe you should turn back.")
+  }
 } else if (choice === "mountains" && !hasTorch) {
   console.log("It's too dark to proceed. You decide to turn back.");
 } else if (choice === "village" || hasMap) {
   console.log("You find your way to the village.");
+  if (hasWeapon === false && hasCharisma === false) {
+    console.log("The villagers take no notice in your arrival.");
+  } else if (hasWeapon === false && hasCharisma === true) {
+    console.log("The villagers are pleased with your arrival!");
+  } else if (hasWeapon === true || hasCharisma === false) {
+    console.log("The villagers are distrustful of your arrival.")
+    const choice2 = readline.question("Do you want to intimidate them? 'yes' or 'no'?");
+    if (choice2 === "Yes") {
+      console.log("You pretend to lunge at a passing villager.")
+    } else if (choice2 === "No") {
+      console.log("You try to find the nearest Tavern.")
+    }
+  }
 } else {
   console.log("You get lost and wander aimlessly.");
 }
